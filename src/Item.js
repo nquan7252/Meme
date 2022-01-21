6aa4f4ec2b25d8109e994f7c6252d75403685b4d
Quan <nquan7252@gmail.com>
2022-01-21 14:41:34 -0800

    new

--- src/Item.js
+++ src/Item.js
@@ -15,7 +15,7 @@ function Item(props) {
         console.log(e.target[0].value)
         console.log(e.target[1].value)
         fetch(url+`template_id=${location.state.id}&username=nquan7252&password=nguyenquan00&boxes[0][text]=${e.target[0].value}&boxes[1][text]=${e.target[1].value}&boxes[2][text]=${e.target[2].value}&boxes[3][text]=${e.target[3].value}`)
-        .then(data=>data.json()).then(json=>window.open(json.data.url))
+        .then(data=>data.json()).then(json=>window.open(json.data.url,'_blank'))
     }
     return <div className='item-image-container'>
         <div>
