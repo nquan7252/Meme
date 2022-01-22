a0d823680e191704e82606d55adc4264ec9e973d
Quan <nquan7252@gmail.com>
2022-01-21 16:21:35 -0800

    new

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ function Home() {
     fetch(url).then(res=>res.json()).then(json=>setData(json));
   },[])
     return <div className="App">
-    <header><h1>Mememe</h1>
+    <header><h1>Meme Maker</h1>
     <h4>by Quan Nguyen</h4>
     </header>
     {data!=null&&<div className='image-container'>{data.data.memes.map((element,index)=> <Image key={index} data={element} item={false}></Image>)}</div>}
